5cd44eff2a4b52610f0fd32d0cd0c32e065ce8a0
strictlex <pislegin2688@gmail.com>
2026-06-09 10:36:09 +0400

    change logik in sm07check

--- tests/smoke/test_sm_07_depersonalization.py
+++ tests/smoke/test_sm_07_depersonalization.py
@@ -73,8 +73,8 @@ def test_sm_07_depersonalization(db_client, test_logger):
 
     # 6. Проверяем логи
     # time.sleep(10)
-    result = db_client.execute("SELECT COUNT(*) FROM PFLB_LOGS WHERE LSTR iLIKE '%Error%';")
+    result = db_client.execute("SELECT COUNT(*) FROM PFLB_LOGS WHERE upper(LSTR) LIKE '%ERROR%';")
     count = result.fetchone()[0]
     assert count == 0, "Ошибки в логах PFLB_LOGS"
 
-    test_logger.info(result, "SM-07: успешно")
+    test_logger.info("SM-07: успешно")
